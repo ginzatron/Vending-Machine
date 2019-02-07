7cf877ea61a4e1e84f123846f3012522322f6072
Allison Debeljak <and21@case.edu>
2019-02-07 11:30:55 -0500

    Allison creates Log class with CreateLog method.

---
+++ Capstone/Classes/Log.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Capstone.Classes
+{
+    public class Log
+    {
+        public void CreateLog(decimal totalBalance, decimal updatedBalance, string logAction)
+        {
+            string logMessage = $"{DateTime.Now.ToString("MM/dd/yyyy HH:mm")} {totalBalance} {updatedBalance} {logAction}";
+
+            try
+            {
+                using (StreamWriter sw = new StreamWriter("Log.txt", true))
+                {
+                    sw.WriteLine($"{logMessage}");
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine($"Error writing to log: {ex.Message}");
+            }
+
+        }
+    }
+}
