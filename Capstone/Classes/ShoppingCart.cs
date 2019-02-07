5eaeac63aae28499b4dcd3166d678b26a71d90a0
Allison Debeljak <and21@case.edu>
2019-02-07 14:40:35 -0500

    Stylecop ShoppingCart fixes.

--- Capstone/Classes/ShoppingCart.cs
+++ Capstone/Classes/ShoppingCart.cs
@@ -1,21 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Capstone.Classes
 {
     public class ShoppingCart
     {
-        List<Item> cart = new List<Item>();
+        private List<Item> cart = new List<Item>();
 
         public void ItemsPurchased(Item item)
         {
             Console.WriteLine($"{item.Name} purchased");
-            cart.Add(item);
+            this.cart.Add(item);
         }
-
-        
-        
     }
-
 }
