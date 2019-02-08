de9b72af682dcbb4d099a04369a483153c2fbfac
Allison Debeljak <and21@case.edu>
2019-02-08 10:01:58 -0500

    Test MakeChange zero balance returns no change.

---
+++ CapstoneTests/MakeChangeTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Capstone.Classes;
+
+
+namespace CapstoneTests
+{
+    [TestClass]
+    public class MakeChangeTests
+    {
+        [TestMethod]
+        public void ZeroBalanceMakesZeroChange()
+        {
+            VendingMachine vendingMachine = new VendingMachine();
+            decimal actualBalance = vendingMachine.MakeChange();
+            Assert.AreEqual(0, actualBalance, "A balance of zero should return no change.");
+        }
+
+    }
+}
