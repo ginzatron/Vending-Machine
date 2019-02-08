7636ce15e1d2152d22dd9887abbce6c2c241140b
Allison Debeljak <and21@case.edu>
2019-02-08 11:11:39 -0500

    add make sound test

---
+++ CapstoneTests/MakeSoundTests.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Capstone.Classes;
+namespace CapstoneTests
+{
+    [TestClass]
+    public class MakeSoundTests
+    {
+        [TestMethod]
+        public void GumMakesChewChewSound()
+        {
+        }
+    }
+}
