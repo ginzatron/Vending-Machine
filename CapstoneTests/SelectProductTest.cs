cb19aff3ed1da042be6aab360e1d4feca93b0a70
Allison Debeljak <and21@case.edu>
2019-02-08 13:04:23 -0500

    fixed small error in select product test

--- CapstoneTests/SelectProductTest.cs
+++ CapstoneTests/SelectProductTest.cs
@@ -13,7 +13,7 @@ namespace CapstoneTests
             vendingMachine.Balance = 10;
             decimal actualBalance = vendingMachine.SelectProduct("A1");
             Assert.AreEqual((decimal)6.95, actualBalance, "Selecting a1 should return a balance of 6.95.");
-            Assert.AreEqual(4, vendingMachine.stock["A1"].Quantity, "Selecting a1 should reduce quantity of the item from 5 to 4.");
+            Assert.AreEqual(4, vendingMachine.Stock["A1"].Quantity, "Selecting a1 should reduce quantity of the item from 5 to 4.");
         }
     }
 }
